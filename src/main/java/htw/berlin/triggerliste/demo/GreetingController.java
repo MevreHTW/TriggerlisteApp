7180775f5bd9a80e100d20db5321d08b91a1b0f1
MevreHTW <91087985+MevreHTW@users.noreply.github.com>
2022-10-22 16:11:10 +0200

    neue Hoffnung Max und Mevre

--- src/main/java/htw/berlin/triggerliste/demo/GreetingController.java
+++ src/main/java/htw/berlin/triggerliste/demo/GreetingController.java
@@ -8,10 +8,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
-    @GetMapping("/greeting")
+ //   @GetMapping("/greeting")
+ //   public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+ //       model.addAttribute("name", name);
+ //       return "greeting";
+
+    @GetMapping("/")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
+
     }
 
 }
